239b3a1d08c59e0eba556054604bb88c04fbf479
Matthew Wire <mjw@mjwconsult.co.uk>
2017-04-15 12:26:28 +0100

    fixmissingcivi fix/document AJAX functions

--- CRM/SmartdebitReconciliation/Page/AJAX.php
+++ CRM/SmartdebitReconciliation/Page/AJAX.php
@@ -13,17 +13,21 @@ class CRM_SmartdebitReconciliation_Page_AJAX
       return;
     }
     $membershipList = CRM_SmartdebitReconciliation_Utils::getContactMemberships($selectedContact);
-    $cRecur     = CRM_SmartdebitReconciliation_Utils::getContactRecurringContributions($selectedContact);
+    $cRecur = CRM_SmartdebitReconciliation_Utils::getContactRecurringContributions($selectedContact);
     $nullMembership = array( 0 => 'No Membership Found');
-    $nullcRecur = array( 0 => 'No Contribution Recur Found');
     $options['membership'] = $membershipList ? $membershipList : $nullMembership;
-    $options['cRecur']     = $cRecur ? $cRecur : $nullcRecur;
+    $options['cRecur'] = $cRecur;
     echo json_encode($options);
     exit;
   }
 
+  /**
+   * Update the list of recurring contributions (cRecurNotLinked is used when membership type "Donation" is selected)
+   */
   static function getNotLinkedRecurringByContactID() {
     $selectedContact = CRM_Utils_Array::value('selectedContact', $_POST);
+
+    // Get contact memberships
     $mParams = array(
       'version'     => 3,
       'sequential'  => 1,
@@ -31,19 +35,22 @@ class CRM_SmartdebitReconciliation_Page_AJAX
     );
     $aMembership = civicrm_api('Membership', 'get', $mParams);
     $membershipWithRecur = array();
+    // Filter memberships that have linked recurring contributions
     foreach ($aMembership['values'] as $membership ) {
       if (!empty($membership['contribution_recur_id'])) {
         $membershipWithRecur [] = $membership['contribution_recur_id'];
       }
     }
+    // Get all recurring contributions for contact
     $allRecurringRecords = $originalAllRecurringRecords = CRM_SmartdebitReconciliation_Utils::getContactRecurringContributions($selectedContact);
+    // Now filter and remove recurring contributions that are linked to a membership for this contact
     foreach ($membershipWithRecur as $linkedRecur) {
       if(array_key_exists($linkedRecur, $allRecurringRecords)) {
         unset($allRecurringRecords[$linkedRecur]);
       }
     }
-    $options['cRecurNotLinked']     = $allRecurringRecords;
-    $options['cRecur']              = $originalAllRecurringRecords;
+    $options['cRecurNotLinked'] = $allRecurringRecords;
+    $options['cRecur'] = $originalAllRecurringRecords;
     echo json_encode($options);
     exit;
   }
